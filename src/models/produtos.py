8f2cc252ce106d09f5e7d105dced9a002d51bf87
Janderson <jjandersonazevedo@gmail.com>
2023-11-27 20:00:59 -0300

    feat: adiciona model de produtos

---
+++ src/models/produtos.py
@@ -0,0 +1,7 @@
+class Produto:
+    def __init__(self, nome, qtdDisponivel, tipo, DataDeChegada, Descricao) -> None:
+        self.nome = nome
+        self.qtdDisponivel = qtdDisponivel
+        self.tipo = tipo
+        self.DataDeChegada = DataDeChegada
+        self.Descricao = Descricao
