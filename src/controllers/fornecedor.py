14c77f4392f17c75f9a69790e8566de8dd57dcf2
Janderson <jjandersonazevedo@gmail.com>
2023-11-28 20:09:42 -0300

    feat: corrige controllers

--- src/controllers/fornecedor.py
+++ src/controllers/fornecedor.py
@@ -3,26 +3,34 @@ from classes_DB_loja import ConexaoDb
 
 class FornecedorController:
     @classmethod
-    def CadastraFornecedor(fornecedor: Fornecedor):
-        (nome, contato, produtos) = fornecedor
+    def CadastraFornecedor(cls, fornecedor: Fornecedor):
+        nome = fornecedor.nome_fornecedor
+        contato = fornecedor.contato_fornecedor
+        produtos = fornecedor.produtos_fornecidos
+
         cursor = ConexaoDb.conn.cursor()
         cursor.execute("INSERT INTO fornecedor (nome_fornecedor, contato_fornecedor, produtos_fornecidos) VALUES (%s, %s, %s);",
                        (nome, contato, produtos))
         ConexaoDb.conn.commit()
-        print("Fornecedor {nome} cadastrado com sucesso.")
-    
+        print(f"Fornecedor {nome} cadastrado com sucesso.")
+
     @classmethod
-    def EditaFornecedor(fornecedor: Fornecedor):
-        (id_fornecedor, nome, contato, produtos) = fornecedor
+    def EditaFornecedor(cls, fornecedor: Fornecedor):
+        id_fornecedor = fornecedor.id_fornecedor
+        nome = fornecedor.nome_fornecedor
+        contato = fornecedor.contato_fornecedor
+        produtos = fornecedor.produtos_fornecidos
+
         cursor = ConexaoDb.conn.cursor()
         cursor.execute("UPDATE fornecedor SET nome_fornecedor = %s, contato_fornecedor = %s, produtos_fornecidos = %s WHERE id_fornecedor = %s;",
                        (nome, contato, produtos, id_fornecedor))
         ConexaoDb.conn.commit()
-        print("Fornecedor alterado para {nome} com sucesso.")
-    
+        print(f"Fornecedor alterado para {nome} com sucesso.")
+
     @classmethod
-    def ExcluiFornecedor(fornecedor: Fornecedor):
+    def ExcluiFornecedor(cls, fornecedor: Fornecedor):
+        id_fornecedor = fornecedor.id_fornecedor
         cursor = ConexaoDb.conn.cursor()
-        cursor.execute("DELETE FROM fornecedor WHERE id_fornecedor = %s;", (fornecedor.id_fornecedor))
+        cursor.execute("DELETE FROM fornecedor WHERE id_fornecedor = %s;", (id_fornecedor,))
         ConexaoDb.conn.commit()
         print("Fornecedor excluído com sucesso.")
--- src/controllers/produto.py
+++ src/controllers/produto.py
@@ -3,26 +3,40 @@ from classes_DB_loja import ConexaoDb
 
 class ProdutoController:
     @classmethod
-    def CadastraProduto(produto: Produto):
-        (nome, qtd_disponivel, tipo, data_chegada, descricao, preco) = produto
+    def CadastraProduto(cls, produto: Produto):
+        nome = produto.nome_produto
+        qtd_disponivel = produto.qtd_disponivel
+        tipo = produto.tipo
+        data_chegada = produto.data_chegada
+        descricao = produto.descricao
+        preco = produto.preco
+
         cursor = ConexaoDb.conn.cursor()
-        cursor.execute("INSERT INTO produtos (nome_produto, qtd_disponivel, tipo, data_chegada, descricao) VALUES (%s, %s, %s, %s, %s, %s);",
+        cursor.execute("INSERT INTO produtos (nome_produto, qtd_disponivel, tipo, data_chegada, descricao, preco) VALUES (%s, %s, %s, %s, %s, %s);",
                        (nome, qtd_disponivel, tipo, data_chegada, descricao, preco))
         ConexaoDb.conn.commit()
-        print("Produto {nome} cadastrado com sucesso.")
-    
-    @classmethod
-    def EditaProduto(produto: Produto):
-        (id_produto, nome, qtd_disponivel, tipo, data_chegada, descricao, preco) = produto
-        cursor = ConexaoDb.conn.cursor()
-        cursor.execute("UPDATE produtos SET nome_produto = %s, qtd_disponivel = %s, tipo = %s, data_chegada = %s, descricao = %s, preco = %s WHERE id_produto = %s;",
-                       (nome, qtd_disponivel, tipo, data_chegada, descricao, id_produto, preco))
-        ConexaoDb.conn.commit()
-        print("Produto alterado para {nome} com sucesso.")
+        print(f"Produto {nome} cadastrado com sucesso.")
 
     @classmethod
-    def ExcluiProduto(produto: Produto):
+    def EditaProduto(cls, produto: Produto):
+        id_produto = produto.id_produto
+        nome = produto.nome_produto
+        qtd_disponivel = produto.qtd_disponivel
+        tipo = produto.tipo
+        data_chegada = produto.data_chegada
+        descricao = produto.descricao
+        preco = produto.preco
+
         cursor = ConexaoDb.conn.cursor()
-        cursor.execute("DELETE FROM produtos WHERE id_produto = %s;", (produto.id_produto))
+        cursor.execute("UPDATE produtos SET nome_produto = %s, qtd_disponivel = %s, tipo = %s, data_chegada = %s, descricao = %s, preco = %s WHERE id_produto = %s;",
+                       (nome, qtd_disponivel, tipo, data_chegada, descricao, preco, id_produto))
+        ConexaoDb.conn.commit()
+        print(f"Produto alterado para {nome} com sucesso.")
+
+    @classmethod
+    def ExcluiProduto(cls, produto: Produto):
+        id_produto = produto.id_produto
+        cursor = ConexaoDb.conn.cursor()
+        cursor.execute("DELETE FROM produtos WHERE id_produto = %s;", (id_produto,))
         ConexaoDb.conn.commit()
         print("Produto excluído com sucesso.")
